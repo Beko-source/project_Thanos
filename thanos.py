63770dea8f3febe4b754d7fe6589b00747bd0a7c
Beko <beboazz45@gmail.com>
2021-07-26 18:34:43 +0200

    v1

--- thanos.py
+++ thanos.py
@@ -1,2 +1,11 @@
-print('hello')
-print('sds')
+import os
+import random
+
+directory = r"C:\Users\pc\Desktop\ASSIG_4\Universe_2"
+
+files_in_directory = os.listdir( directory )
+files_in_directory=( random.sample ( files_in_directory , 25 ) )
+filtered_files = [file for file in files_in_directory if file.endswith(".png")]
+for file in filtered_files:
+    path_to_file = os.path.join ( directory , file )
+    os.remove( path_to_file )
